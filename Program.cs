dd08894db59cb974e7283e2efcb851176804773b
ordomilo-code <loris.protin@ordomilo.com>
2026-03-29 21:24:20 +0200

    apply ef migrations on non-dev startup

--- Program.cs
+++ Program.cs
@@ -88,6 +88,7 @@ app.UseExceptionHandler(errorApp =>
 if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
+    await ProductionDatabaseMigrator.MigrateAsync(app.Services, app.Logger);
 }
 
 if (app.Environment.IsDevelopment())
